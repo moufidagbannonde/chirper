74764aaa05d2665cd2a336178213d1c3f3f2411f
Moufid Agbannonde <magbannonde@gmail.com>
2024-07-31 19:17:33 +0200

    ✔

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -16,14 +16,38 @@ const form = useForm({
  
     <AuthenticatedLayout>
         <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-            <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
-                <textarea
-                    v-model="form.message"
-                    placeholder="What's on your mind?"
-                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                ></textarea>
-                <InputError :message="form.errors.message" class="mt-2" />
-                <PrimaryButton class="mt-4">Chirp</PrimaryButton>
+            <form action="{{route("todo.store")}}" method="post">
+                @csrf
+                <label for="name" class="form-label mt-4">Nom</label>
+                <!-- mt-4 = margin 4 -->
+                <input type="text" name="name" class="form-control" id="">
+                <div class="text-danger">
+                    @error('name')
+                        {{$message}}
+                    @enderror
+                </div>
+                <label for="description" class="form-label mt-4">Contenu</label>
+                <textarea name="description" class="form-control" id="" cols="30" rows="6"></textarea>
+                <div class="text-danger">
+                    @error('description')
+                        {{$message}}
+                    @enderror
+                </div>
+                <label for="author" class="form-label mt-4">Auteur</label>
+                <input type="text" name="author" class="form-control" id="">
+                <div class="text-danger">
+                    @error('author')
+                        {{$message}}
+                    @enderror
+                </div>
+                <label for="date" class="form-label mt-4"> Date</label>
+                <input type="date" name="date" class="form-control" id="">
+                <div class="text-danger">
+                    @error('date')
+                        {{$message}}
+                    @enderror
+                </div>
+                <button class="btn btn-primary btn-lg mt-4">Ajouter une tâche</button>
             </form>
             <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
                 <Chirp
